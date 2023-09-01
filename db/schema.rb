2fd40859b761f446bc320ced52f384e7acd9ab56
warawarakid <sei.shogun@gmail.com>
2023-09-02 01:37:09 +0900

    モデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_09_01_030201) do
+ActiveRecord::Schema.define(version: 2023_09_01_151855) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "color_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2023_09_01_030201) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
